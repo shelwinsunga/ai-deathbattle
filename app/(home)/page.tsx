2cce3dd9fe7d1bb7d9ca30b8b4face544701ac4b
Shelwin <shelwinsunga@gmail.com>
2023-08-26 13:21:09 -0500

    landing-page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,10 +2,29 @@
 
 import Link from "next/link";
 import { useCursors } from "./cursors-provider";
+import NewRoom from "../chat/components/NewRoom";
+import { generateSlug, RandomWordOptions } from "random-word-slugs";
+import { RoomInfo, SINGLETON_ROOM_ID } from "@/party/chatRooms";
+import { PARTYKIT_URL } from "@/app/env";
+
+
+const randomWords: RandomWordOptions<3> = {
+  format: "kebab",
+  categories: { noun: ["animals"] },
+  partsOfSpeech: ["adjective", "adjective", "noun"],
+};
+
+const partyUrl = `${PARTYKIT_URL}/parties/chatrooms/${SINGLETON_ROOM_ID}`;
+
+export const revalidate = 0;
+
+
 
 export default function Home() {
   const { getCount } = useCursors();
   const count = getCount();
+
+  
 
   return (
     <div className="w-full flex flex-col gap-2 items-center justify-center text-center">
@@ -13,12 +32,7 @@ export default function Home() {
       <section className="flex flex-col gap-2">
         <h1 className="text-4xl font-medium pb-6">AI Death Battle</h1>
       </section>
-
-      <Link href="/chat" className="underline">
-        <button className="flex items-center justify-center px-10 py-6 border border-stone-200 rounded-lg shadow hover:shadow-md">
-          Create Room -&gt;
-        </button>
-      </Link>
+      <NewRoom slug={generateSlug(3, randomWords)} />
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,10 @@ import { Inter } from "next/font/google";
 import { Providers } from "./components/Providers";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import NewRoom from "./chat/components/NewRoom";
+import { generateSlug, RandomWordOptions } from "random-word-slugs";
+import { RoomInfo, SINGLETON_ROOM_ID } from "@/party/chatRooms";
+import { PARTYKIT_URL } from "@/app/env";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +15,16 @@ export const metadata: Metadata = {
   title: "Demo",
   description: "AI Death Battle",
 };
+
+const randomWords: RandomWordOptions<3> = {
+  format: "kebab",
+  categories: { noun: ["animals"] },
+  partsOfSpeech: ["adjective", "adjective", "noun"],
+};
+
+const partyUrl = `${PARTYKIT_URL}/parties/chatrooms/${SINGLETON_ROOM_ID}`;
+
+export const revalidate = 0;
 
 export default function RootLayout({
   children,
